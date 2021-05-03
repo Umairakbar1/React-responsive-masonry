e8bbf0d08d13726b1c9f23b43e43f4ec816b20d4
Umair Akbar <ua5915727@gmail.com.com>
2021-05-03 23:22:54 +0500

    make it better

--- src/Components/ResponsiveMasonry/responsiveManosry.js
+++ src/Components/ResponsiveMasonry/responsiveManosry.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
+//      importing Images
 import Image1 from "../../Assests/Images/1.jpg";
 import Image2 from "../../Assests/Images/2.jpg";
 import Image3 from "../../Assests/Images/3.jpg";
 import Image4 from "../../Assests/Images/4.jpg";
-
+//      importing css files
+import "./responsiveMasonry.css";
 const images = [
     Image1,
     Image2,
@@ -16,40 +18,34 @@ const images = [
 ]
 class ResponsiveManosry extends Component {
     state = {  }
+    componentDidMount(){
+        if(images%2==0){
+            document.getElementsByTagName("img").className="masonry-containerss"
+        }
+        else{
+            
+        }
+    }
     render() { 
         return ( 
             <React.Fragment>
-            <div  style={{width:"20%",margin:"auto"}}>
+            <div className="masonry-container">
                 <p style={{textAlign:"left",fontWeight:"600",color:"#404043",marginTop:"10px"}}>Main Public album</p>
             <ResponsiveMasonry
                 columnsCountBreakPoints={{10: 1, 400: 2}}
-
                 columnsCount={2}
             >
                 <Masonry gutter="8px">
-                    {/* {images.map((image, i) => ( */}
+                    {images.map((image, i) => (
                         <img
-                            
-                            src={Image1}
-                            style={{width: "100%",height:"120px", display: "block",borderRadius:"10px"}}
+                            id="image"
+                            key={i}
+                            src={image}
+                            style={i%2?{width: "100%",height:"150px", display: "block",borderRadius:"10px"}:{width: "100%",height:"120px", display: "block",borderRadius:"10px"}}
                             alt=""
                         />
-                        <img
-                            src={Image2}
-                            style={{width: "100%", height:"150px",display: "block",borderRadius:"10px"}}
-                            alt=""
-                        />
-                        <img
-                            src={Image3}
-                            style={{width: "100%",height:"120px", display: "block",borderRadius:"10px"}}
-                            alt=""
-                        />
-                            <img
-                            src={Image4}
-                            style={{width: "100%",height:"150px", display: "block",borderRadius:"10px"}}
-                            alt=""
-                        />
-                    {/* ))} */}
+
+                     ))} 
                 </Masonry>
             </ResponsiveMasonry>
             </div>
