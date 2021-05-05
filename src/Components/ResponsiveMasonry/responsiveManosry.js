c62591da7fe0619b72130a1488f49cfa7b343466
Umair Akbar <ua5915727@gmail.com.com>
2021-05-06 04:24:20 +0500

    make it bettr

--- src/Components/ResponsiveMasonry/responsiveManosry.js
+++ src/Components/ResponsiveMasonry/responsiveManosry.js
@@ -8,80 +8,62 @@ import Image4 from "../../Assests/Images/4.jpg";
 //      importing css files
 import "./responsiveMasonry.css";
 const images = [
-    {
-        imgs:Image1,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image2,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image3,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image4,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image1,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image2,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image3,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image4,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image1,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image2,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image3,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image4,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image1,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image2,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    },
-    {
-        imgs:Image3,
-        CSS:{height:"120px",width: "100%",display: "block",borderRadius:"10px"}
-    }
-    ,
-    {
-        imgs:Image4,
-        CSS:{height:"150px",width: "100%",display: "block",borderRadius:"10px"}
-    }
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image1,
+    Image2,
+    Image3,
+    Image4,
 ]
 class ResponsiveManosry extends Component {
-    state = {  }
-    componentDidMount(){
-        if(images%2==0){
-            document.getElementsByTagName("img").className="masonry-containerss"
+    state = { 
+        oddNumber:false,
+        evenNumber:false
+     }
+    handleProvideStyle=(index)=>{
+        let toggle = false;
+        if(index%2===0)
+        {
+            if(this.state.oddNumber===false){
+                this.state.oddNumber=true;
+                return {width: "100%",height:"150px", display: "block",borderRadius:"10px"};
+            }
+            else{
+                this.state.oddNumber=false;
+                return {width: "100%",height:"120px", display: "block",borderRadius:"10px"};
+            }
         }
         else{
-            
+            if(this.state.evenNumber===false){
+                this.state.evenNumber=true;
+                return {width: "100%",height:"120px", display: "block",borderRadius:"10px"};
+            }
+            else{
+                this.state.evenNumber=false;
+                return {width: "100%",height:"150px", display: "block",borderRadius:"10px"};
+            }
         }
     }
     render() { 
@@ -89,54 +71,19 @@ class ResponsiveManosry extends Component {
             <React.Fragment>
             <div className="masonry-container">
                 <p style={{textAlign:"left",fontWeight:"600",color:"#404043",marginTop:"10px"}}>Main Public album</p>
-            {/* <ResponsiveMasonry
-                columnsCountBreakPoints={{0: 1, 10: 2}}
-                columnsCount={2}
-            > */}
                 <Masonry gutter="8px" columnsCount={2}>
                     {images.map((image, i) => (
                         <div>
                         <img
-                            id="image"
+                            id="imageid"
                             key={i}
-                            src={image.imgs}
-                            style={image.CSS}
+                            src={image}
+                            style={this.handleProvideStyle(i)}
                             alt=""
                         />
                         </div>
                      ))} 
                 </Masonry>
-                {/* <Masonry gutter="auto" columnsCount={2}>
-                    {images.map((image, i) => (
-                        <div>
-                            <div >
-                            {
-                            i%2===0?
-                            <div >
-                            <img
-                            id="image"
-                            key={i}
-                            src={image}
-                            style={{width: "100%",margin:"10px 2px 0px -10px",height:"120px", display: "block",borderRadius:"10px"}}
-                            alt=""
-                            />
-                            </div>
-                            :
-                            <img
-                            id="image"
-                            key={i}
-                            src={image}
-                            style={i%2?{width: "100%",height:"149px",margin:"10px 2px 0px 0px", display: "block",borderRadius:"10px"}:{width: "100%",height:"120px",marginTop:"auto", display: "block",borderRadius:"10px"}}
-                            alt=""
-                            />
-                        }
-                            </div>
-                        </div>
-
-                     ))}
-                      
-                </Masonry> */}
-            {/* </ResponsiveMasonry> */}
             </div>
             </React.Fragment>
          );
